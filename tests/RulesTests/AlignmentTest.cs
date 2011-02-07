81d2fe6b98696543df832a76ca82bab9c476c679
Sebastian Hartte <sebastian@hartte.de>
2011-02-07 22:54:05 +0100

    Added rules tests

---
+++ tests/RulesTests/AlignmentTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Rules;
+
+namespace RulesTests
+{
+    class AlignmentTest
+    {
+        [Test]
+        public void TestFromLegacyString()
+        {
+            Assert.AreEqual(Alignment.TrueNeutral, Alignments.ParseLegacyString("true_neutral"));
+            Assert.AreEqual(Alignment.ChaoticEvil, Alignments.ParseLegacyString("chaotic_evil"));
+
+            Assert.Throws<InvalidOperationException>(() => Alignments.ParseLegacyString("NO_SUCH_ALIGNMENT"));
+        }
+
+        [Test]
+        public void TestToLegacyString()
+        {
+            Assert.AreEqual("true_neutral", Alignment.TrueNeutral.ToLegacyString());
+            Assert.AreEqual("chaotic_evil", Alignment.ChaoticEvil.ToLegacyString());
+        }
+    }
+}
---
+++ tests/RulesTests/RaceTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+using NUnit.Framework;
+using Rules;
+
+namespace RulesTests
+{
+    class RaceTest
+    {
+        private static Stream GetResource(string name)
+        {
+            var stream = typeof(RaceTest).Assembly.GetManifestResourceStream(name);
+            if (stream == null)
+                throw new FileNotFoundException("Resource not found.", name);
+            return stream;
+        }
+
+        [Test]
+        public void TestSerialization()
+        {
+            var serializer = new XmlSerializer(typeof (Race));
+
+            var reader = new XmlTextReader(GetResource("RulesTests.RulesTest1.xml"));
+
+            // Read to document start
+            while (reader.Read())
+            {
+                if (reader.NodeType == XmlNodeType.Element && "Race".Equals(reader.LocalName))
+                {
+                    var r = (Race)serializer.Deserialize(reader.ReadSubtree());
+                    Console.WriteLine(r.Id);
+                    Console.WriteLine(r.Name);
+
+                    Console.WriteLine(r.MaleCharacteristics.Height);
+                }
+            }
+        }
+
+    }
+}
